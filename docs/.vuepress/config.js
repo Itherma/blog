53a9eace0610c50b19c8f46baaa47044b0457374
microjan <1227971544@qq.com>
2022-03-21 20:25:41 +0800

    edit config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
   theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   title: "Itherma's blog",
-  description: 'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。',
+  description: 'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,react,css3,html5,Node,git,github等技术文章。',
   base: '/blog/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
@@ -38,8 +38,8 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        '@as': resolve(__dirname, './assets'),
-        '@imgs': resolve(__dirname, './assets/imgs')
+        '@as': resolve(__dirname, 'assets'),
+        '@imgs': resolve(__dirname, 'assets/imgs')
       }
     }
   }
